58026c95059043b8a367304ea3a2c3f159f77013
glmckay <glmckay@uwaterloo.ca>
2023-04-22 23:40:24 -0400

    We should have an error if the input contains the concatenation character. I wanted the error to contain the index, so it couldn't be a static str and I thought wrapping the error message in an Error struct might be more idiomatic.

--- src/modules/parser.rs
+++ src/modules/parser.rs
@@ -1,49 +1,69 @@
 use super::automata::{closure, concat, optional, or, plus, Automata};
 use super::state::Anchor;
 
+#[derive(Debug)]
+pub struct Error {
+    #[allow(dead_code)]
+    msg: String,
+}
+
+impl Error {
+    pub fn from(msg: &str) -> Error {
+        Error {
+            msg: String::from(msg),
+        }
+    }
+}
+
 const CONCAT_CHAR: char = 27 as char;
 const POP_ERR: &str = "error popping from stack";
 
-pub fn parse(expr: &str) -> Result<Automata, &'static str> {
-    let postfix = to_postfix(expr);
-        let mut automata_stack = Vec::<Automata>::new();
+pub fn parse(expr: &str) -> Result<Automata, Error> {
+    let postfix = to_postfix(expr)?;
+    let mut automata_stack = Vec::<Automata>::new();
 
-        for c in postfix.chars() {
-            match c {
-                '^' => automata_stack.push(Automata::from_anchor(Anchor::Start)),
-                '$' => automata_stack.push(Automata::from_anchor(Anchor::End)),
-                '|' => {
-                    let b = automata_stack.pop().ok_or(POP_ERR)?;
-                    let a = automata_stack.pop().ok_or(POP_ERR)?;
-                    automata_stack.push(or(a, b));
-                }
-                CONCAT_CHAR => {
-                    let b = automata_stack.pop().ok_or(POP_ERR)?;
-                    let a = automata_stack.pop().ok_or(POP_ERR)?;
-                    automata_stack.push(concat(a, b));
-                }
-                '*' => {
-                    let a = automata_stack.pop().ok_or(POP_ERR)?;
-                    automata_stack.push(closure(a));
-                }
-                '?' => {
-                    let a = automata_stack.pop().ok_or(POP_ERR)?;
-                    automata_stack.push(optional(a));
-                }
-                '+' => {
-                    let a = automata_stack.pop().ok_or(POP_ERR)?;
-                    automata_stack.push(plus(a));
-                }
-                '.' => automata_stack.push(Automata::from_lambda(|_| true)),
-                _ => automata_stack.push(Automata::from_token(c)),
+    for c in postfix.chars() {
+        match c {
+            '^' => automata_stack.push(Automata::from_anchor(Anchor::Start)),
+            '$' => automata_stack.push(Automata::from_anchor(Anchor::End)),
+            '|' => {
+                let b = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                let a = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                automata_stack.push(or(a, b));
             }
+            CONCAT_CHAR => {
+                let b = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                let a = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                automata_stack.push(concat(a, b));
+            }
+            '*' => {
+                let a = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                automata_stack.push(closure(a));
+            }
+            '?' => {
+                let a = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                automata_stack.push(optional(a));
+            }
+            '+' => {
+                let a = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                automata_stack.push(plus(a));
+            }
+            '.' => automata_stack.push(Automata::from_lambda(|_| true)),
+            _ => automata_stack.push(Automata::from_token(c)),
         }
+    }
 
-        automata_stack.pop().ok_or("Error parsing expression given!")
+    if automata_stack.len() > 1 {
+        Err(Error::from("Interal error: final stack is too large"))
+    } else {
+        automata_stack
+            .pop()
+            .ok_or(Error::from("Interal error: final stack is empty"))
+    }
 }
 
-fn to_postfix(expr: &str) -> String {
-    let modified_expr = add_concat_char(expr);
+fn to_postfix(expr: &str) -> Result<String, Error> {
+    let modified_expr = add_concat_char(expr)?;
     let mut postfix = String::new();
     let mut operator_stack = Vec::<char>::new();
     let precedence = |c| match c {
@@ -57,7 +77,9 @@ fn to_postfix(expr: &str) -> String {
             '|' | CONCAT_CHAR | '*' | '?' | '+' => {
                 let curr_precedence = precedence(c);
                 while let Some(&op) = operator_stack.last() {
-                    if (op == '(') || (precedence(op) < curr_precedence) { break; }
+                    if (op == '(') || (precedence(op) < curr_precedence) {
+                        break;
+                    }
                     operator_stack.pop();
                     postfix.push(op);
                 }
@@ -66,7 +88,9 @@ fn to_postfix(expr: &str) -> String {
             '(' => operator_stack.push(c),
             ')' => {
                 while let Some(token) = operator_stack.pop() {
-                    if token == '(' { break; }
+                    if token == '(' {
+                        break;
+                    }
                     postfix.push(token);
                 }
             }
@@ -78,10 +102,16 @@ fn to_postfix(expr: &str) -> String {
         postfix.push(op);
     }
 
-    postfix
+    Ok(postfix)
 }
 
-fn add_concat_char(expr: &str) -> String {
+fn add_concat_char(expr: &str) -> Result<String, Error> {
+    if let Some(pos) = expr.find(CONCAT_CHAR) {
+        return Err(Error {
+            msg: format!("Illegal character at index {}", pos),
+        });
+    }
+
     let mut result = String::new();
     let mut it = expr.chars();
     if let Some(first) = it.next() {
@@ -98,5 +128,5 @@ fn add_concat_char(expr: &str) -> String {
         }
     }
 
-    result
+    Ok(result)
 }
--- src/modules/regexp.rs
+++ src/modules/regexp.rs
@@ -1,12 +1,12 @@
 
-use super::{parser::{parse}, automata::Automata};
+use super::{parser::{parse, Error}, automata::Automata};
 
 pub struct RegExp {
     automata: Automata,
 }
 
 impl RegExp {
-    pub fn new(expr: &str) -> Result<Self, &'static str> {
+    pub fn new(expr: &str) -> Result<Self, Error> {
         Ok( RegExp{ automata: parse(expr)? } )
     }
 
