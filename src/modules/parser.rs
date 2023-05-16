b4ffb2fe03e7007cf80ce6db209a7af38cc65eca
njhlai <nicholasjhlai@gmail.com>
2023-05-15 21:36:56 -0700

    Added escaped character parsing

--- src/modules/parser.rs
+++ src/modules/parser.rs
@@ -9,12 +9,13 @@ pub fn parse(expr: &str) -> Result<Automata, Error> {
     let postfix = to_postfix(expr)?;
     let mut automata_stack = Vec::<Automata>::new();
 
-    for c in postfix.chars() {
+    let mut it = postfix.chars();
+    while let Some(c) = it.next() {
         match c {
             '^' => automata_stack.push(Automata::from_anchor(Anchor::Start)),
             '$' => automata_stack.push(Automata::from_anchor(Anchor::End)),
             '|' => {
-                let b = automata_stack.pop().ok_or(Error::from(POP_ERR))?;
+                let b = automata_stack.pop().ok_or_else(|| Error::from(POP_ERR))?;
                 let a = automata_stack.pop().ok_or_else(|| Error::from(POP_ERR))?;
                 automata_stack.push(a.or(b));
             }
@@ -36,6 +37,27 @@ pub fn parse(expr: &str) -> Result<Automata, Error> {
                 automata_stack.push(a.plus());
             }
             '.' => automata_stack.push(Automata::from_lambda(|_| true)),
+            '\\' => {
+                if let Some(d) = it.next() {
+                    match d {
+                        'b' => automata_stack.push(Automata::from_anchor(Anchor::WordBoundary)),
+                        'd' => automata_stack.push(Automata::from_lambda(|x| x.is_ascii_digit())),
+                        'D' => automata_stack.push(Automata::from_lambda(|x| !x.is_ascii_digit())),
+                        'w' => automata_stack.push(Automata::from_lambda(|x| x.is_ascii_alphanumeric())),
+                        'W' => automata_stack.push(Automata::from_lambda(|x| !x.is_ascii_alphanumeric())),
+                        's' => automata_stack.push(Automata::from_lambda(|x| x.is_ascii_whitespace())),
+                        'S' => automata_stack.push(Automata::from_lambda(|x| !x.is_ascii_whitespace())),
+                        '^' | '$' | '|' | '*' | '?' | '+' | '.' | '\\' => automata_stack.push(Automata::from_token(d)),
+                        't' => automata_stack.push(Automata::from_token('\t')),
+                        'n' => automata_stack.push(Automata::from_token('\n')),
+                        'r' => automata_stack.push(Automata::from_token('\r')),
+                        'v' => automata_stack.push(Automata::from_token('\x0b')),
+                        'f' => automata_stack.push(Automata::from_token('\x0c')),
+                        '0' => automata_stack.push(Automata::from_token('\0')),
+                        _ => return Err(Error::from(format!("Unknown escaped character found: {d}").as_str())),
+                    }
+                }
+            }
             _ => automata_stack.push(Automata::from_token(c)),
         }
     }
@@ -59,8 +81,13 @@ fn to_postfix(expr: &str) -> Result<String, Error> {
         _ => 2,
     };
 
-    for c in modified_expr.chars() {
+    let mut it = modified_expr.chars();
+    while let Some(c) = it.next() {
         match c {
+            '\\' => {
+                postfix.push(c);
+                postfix.push(it.next().ok_or(Error::from("\\ does not escape anything"))?);
+            }
             '|' | CONCAT_CHAR | '*' | '?' | '+' => {
                 let curr_precedence = precedence(c);
                 while let Some(&op) = operator_stack.last() {
@@ -104,7 +131,7 @@ fn add_concat_char(expr: &str) -> Result<String, Error> {
         let mut prev = first;
 
         for c in it {
-            if !matches!(prev, '(' | '|') && !matches!(c, ')' | '|' | '*' | '?' | '+') {
+            if !matches!(prev, '(' | '|' | '\\') && !matches!(c, ')' | '|' | '*' | '?' | '+') {
                 result.push(CONCAT_CHAR);
             }
 
